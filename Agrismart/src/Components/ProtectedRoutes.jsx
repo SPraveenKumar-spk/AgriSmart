b7c9d809a8b296299262ea67b283f8dc65c94c23
S. Praveen Kumar <109852619+SPraveenKumar-spk@users.noreply.github.com>
2024-09-08 10:20:32 +0530

    Update ProtectedRoutes.jsx

--- Agrismart/src/Components/ProtectedRoutes.jsx
+++ Agrismart/src/Components/ProtectedRoutes.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../store/AuthContext";
 
 function ProtectedRoutes() {
-  const { isLoggedIn } = useAuth();
+  const  isLoggedIn = true
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
 }
 
